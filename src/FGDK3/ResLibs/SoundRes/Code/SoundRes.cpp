14148e3e9e3382adb970087f3560b13db54123de
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-01-12 18:54:31 -0500

    Add split for SoundRes.cpp

---
+++ src/FGDK3/ResLibs/SoundRes/Code/SoundRes.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+
+#include "FGDK3/Playstation2/Thread.h"
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", func_0024BA70);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", SoundRes_InternalInitialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", SoundRes_InternalFinalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", SoundRes_Initialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", SoundRes_Finalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", func_0024BB58);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", func_0024BDB0);
+
+INCLUDE_RODATA("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", D_00432FB0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", __tf15Sound_Resources);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", func_0024BE68);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/ResLibs/SoundRes/Code/SoundRes", __tft8Resource1Z12Sound_Effect);
