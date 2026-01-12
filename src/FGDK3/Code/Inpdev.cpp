7e99b660958e202fa369f12cae664dbfd363d643
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-01-12 14:46:25 -0500

    Add split for Inpdev.cpp

---
+++ src/FGDK3/Code/Inpdev.cpp
@@ -0,0 +1,53 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", InputDevice_InternalInitialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", InputDevice_InternalFinalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", InputDevice_Initialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", InputDevice_Finalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FB980);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FB9B0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBA58);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBAA8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBAD0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBB70);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBB78);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBB80);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBC20);
+
+INCLUDE_RODATA("asm/nonmatchings/FGDK3/Code/Inpdev", _vt$18InputDevice_Device);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", __tf18InputDevice_Device);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCA0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCA8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCB0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCB8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCC0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCC8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCD0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCD8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCE0);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBCE8);
+
+INCLUDE_ASM("asm/nonmatchings/FGDK3/Code/Inpdev", func_002FBD20);
---
+++ src/text_002FBD40.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+
+// inputCD
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBD40);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBD70);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBE78);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBEB0);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBEE8);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBEF0);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBF18);
+
+INCLUDE_RODATA("asm/nonmatchings/text_002FBD40", D_00447228);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", __tf11InputCD_PS2);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", func_002FBF98);
+
+INCLUDE_ASM("asm/nonmatchings/text_002FBD40", __tf7InputCD);
