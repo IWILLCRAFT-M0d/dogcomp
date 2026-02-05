ad92318e4a2b442c025bb0b509eeeb1c2bc296f3
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 10:03:22 -0500

    Add split for LevelGate

---
+++ src/Dogs/Code/LevelGate.cpp
@@ -0,0 +1,47 @@
+#include "common.h"
+#include "unk.h"
+
+// LevelGate
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F1F8); /* LevelGate::LevelGate */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F2A8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD1C8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD1F0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD220);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F380);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F420);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F680);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013F808);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FAD0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FB18);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD298);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD2B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/LevelGate", D_003AD2C8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FB70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", __tf9LevelGate);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD08);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD10);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD20);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD28);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/LevelGate", func_0013FD48);
