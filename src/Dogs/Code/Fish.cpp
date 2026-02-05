5d4f0bb0b1d1126cd21d6691f6c8bb9e68c92690
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 09:10:37 -0500

    Split for Fish

---
+++ src/Dogs/Code/Fish.cpp
@@ -0,0 +1,66 @@
+#include "common.h"
+
+#include "unk.h"
+
+// Fish
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B288); /* Fish::Fish */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B3A0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B440);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B550);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B570);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B590);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B640);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013B790);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013BDE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013BE70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013BEB0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A87DC);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8808);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8830);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8850);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8868);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8880);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A8898);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A88B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A9AD0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A9B10);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A9B28);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Fish", D_003A9B40);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013BF40);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C1E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C238);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C290); /* return D_0044cf60; */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C298);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C2B8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C308);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Fish", func_0013C348);
