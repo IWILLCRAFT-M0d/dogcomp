2d1485f665e3cb2cf860dabfbf2226ae23cb6d78
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 09:16:52 -0500

    Add split for FlyingHat

---
+++ src/Dogs/Code/FlyingHat.cpp
@@ -0,0 +1,55 @@
+#include "common.h"
+#include "unk.h"
+
+// FlyingHat
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C368);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C460);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C518);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C528);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C530);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013C570);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CAD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CC20);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003A9FD0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003A9FE8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA010);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA030);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA048);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA060);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA078);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AA090);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AB2B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AB2F0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AB308);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/FlyingHat", D_003AB320);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CC88);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CDC8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CDF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CE18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CE70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/FlyingHat", func_0013CE78);
