8494e0ed3df0e327ce255995aa6b42e6d88ac6a1
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 12:29:37 -0500

    Add split for Rat.cpp

---
+++ src/Dogs/Code/Rat.cpp
@@ -0,0 +1,67 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014F640);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014F7A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014F7C8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014F818);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014F9B0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_0014FAE0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150120);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_001503B0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_001505D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150648);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150650);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150660);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150668);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150B78);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7DD0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7DE8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E10);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E30);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E48);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E60);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E78);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B7E90);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B90B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B90F0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B9108);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Rat", D_003B9120);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150D08);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150D58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", __tf3Rat);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150E68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150EE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150EF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Rat", func_00150F10);
