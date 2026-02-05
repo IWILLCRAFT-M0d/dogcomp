ecfeab985be47863d8f4b64b5db898e5f6369f26
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 13:32:39 -0500

    Add split for SkiLiftSeat

---
+++ src/Dogs/Code/SkiLiftSeat.cpp
@@ -0,0 +1,59 @@
+#include "common.h"
+#include "debug.h"
+
+#include "Dogs/WorldObj.h"
+#include "unk.h"
+#include <string.h>
+
+// SkiLiftSeat
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016AEE0); /* SkiLiftSeat::SkiLiftSeat */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B040);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B110);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B238);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B290);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B2B0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B2D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B480);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016B4A8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C1220);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C1248);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C1268);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C1280);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C1298);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C12B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C12C8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C24E8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C2528);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C2540);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SkiLiftSeat", D_003C2558);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016BB90);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016BCD0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", __tf11SkiLiftSeat);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016BD50);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016BD58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SkiLiftSeat", func_0016BD60);
