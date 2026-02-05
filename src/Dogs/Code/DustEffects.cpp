a1cd9515e01b59bc349d8878d70d76de2b128059
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 09:02:36 -0500

    Create split for DustEffects

---
+++ src/Dogs/Code/DustEffects.cpp
@@ -0,0 +1,31 @@
+#include "common.h"
+#include "unk.h"
+
+// DustEffectManager (DustEffects.h/cpp)
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013A8F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013ABD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013AC80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013AD58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B030);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B0A0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/DustEffects", D_003A83D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/DustEffects", D_003A83E8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/DustEffects", D_003A8400);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B0C0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", __tf17DustEffectManager);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B258);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B260);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/DustEffects", func_0013B268);
