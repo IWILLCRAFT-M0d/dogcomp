c2128c984cddab5d7dd4060a3b35f1c83df9147b
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 13:07:50 -0500

    Add split for PlayerDog

---
+++ src/Dogs/Code/PlayerDog.cpp
@@ -0,0 +1,122 @@
+#include "common.h"
+
+// PlayerDog
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D410);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D678);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D740);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D7C0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D830);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D8A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014D968);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DA60);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B4E18);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B4E40);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B4E70);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B4EE0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DA68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DB48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DB58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DB80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DBA0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DBC0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DCB8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DCF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014DD20);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014EB88);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014EBA0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014EE70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014EE80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F0A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F188);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F1F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B4FF0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5018);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5038);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5050);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5068);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5080);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B5098);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B62B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B62F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6310);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6328);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B65E0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B65F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6620);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6648);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6668);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6680);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B6698);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B66B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B66C8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B78E8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B7928);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B7940);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/PlayerDog", D_003B7958);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F250);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F498);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", __tf9PlayerDog);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F518);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F520);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", __tf18RoverSaysPuzzleGod);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F618);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/PlayerDog", func_0014F620);
