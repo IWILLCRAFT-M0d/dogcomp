5eec1c2010f1d08d23ef7200a24dc076352d86b8
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 20:05:59 -0500

    Add split for WaterSplash

---
+++ src/Dogs/Code/WaterSplash.cpp
@@ -0,0 +1,104 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017D340);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017D4B8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017D588);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017D650);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017DC70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E120);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E188);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E460);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E4B0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E898);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017E900);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017EBA0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017EBF8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F300);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F468); /* StaticWaterRipple::StaticWaterRipple */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F528);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F568);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F588);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017F728); /* WaterSplashManager::WaterSplashManager */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FA38);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FAD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FB78);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FBD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FBF8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FC30);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FC68);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDE48);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDE60);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDE78);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDE90);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDEA8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDEC0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CDED8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CE080);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CE098);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CE0B0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FCA0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FEE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FF18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FF20);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", __tf18WaterSplashManager);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FF80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FF88);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", __tf17StaticWaterRipple);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FFE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_0017FFF0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WaterSplash", D_003CE2B8); /* "22BaseWaterEffectManager" */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", __tf13RippleManager);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_001800A0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", __tf20CurtainSplashManager);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WaterSplash", func_00180180);
+
