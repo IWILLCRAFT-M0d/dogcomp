ed9eee1759102c40df725e8af3302e4ee27cedff
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-06 13:33:57 -0500

    Add split for text_0022FAC8, text_0023E698

---
+++ src/text_0023E698.cpp
@@ -0,0 +1,129 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023E698);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023E798);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023E7B8);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023E850);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023E968);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EC08);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EC20);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EC90);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EC98);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023ECB0);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EF28);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023EF90);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023F2C0);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023F480);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023F840);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023FBE0);
+
+INCLUDE_ASM("asm/nonmatchings/text_0023E698", func_0023FC90);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432560);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432580);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004325B0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004325E8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432728);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432760);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432770);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432780);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432790);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004327B0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004327D0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004327F0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432810);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432830);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432850);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432878);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432898);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004328B8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004328D8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004328F0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432900);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432928);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432948);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432968);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432980);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432998);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004329A8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004329C0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_004329D8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432A50);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432A68);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432A80);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432A98);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432AB0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432AC8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432AD8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432AF0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B08);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B20);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B30);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B40);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B50);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B68);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B80); /* "13bad_exception" */
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432B90); /* "9exception" */
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432BA0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_0023E698", D_00432BA8);
