77374ea734858674805e0b3bb9169b65b9da7d3c
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 22:15:56 -0500

    Add split for Cloth

---
+++ src/Dogs/Code/Cloth.cpp
@@ -0,0 +1,151 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00124EF8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001250E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001251E0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00125240);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1B90);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1BB8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1BE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00125270);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00125F48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00125F68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126098);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1C60); /* "c:/coding/dogs/Code/Common/Cloth.cpp" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1C88); /* "index+1 < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1CA8); /* "index2 < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1CC0); /* "m_numStretchForces == f" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1CD8); /* "index < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1CF0); /* "index3 < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1D08); /* "(m_numStretchForces + m_numShearForces) == f" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1D38); /* "(index+2) < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1D58); /* "(index2) < m_numParticles" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1D78); /* "m_numForces == f" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1D90); /* "m_numTris == t" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1DA0); /* "f->m_p0->m_numConnected < (MAX_CONNECTIONS+1)" */
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1DD0); /* "f->m_p1->m_numConnected < (MAX_CONNECTIONS+1)" */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126118);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126498);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001265A0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001268A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126970);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126B80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126BD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126D78);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00126E10);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001270F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001272E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001273C0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127958);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001279A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_001279B8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127B48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127B70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127B78);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", __tf5Cloth);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127BD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127BE0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127BE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127BF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127BF8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C00);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C08);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C10);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C20);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C28);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C30);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C38);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C40);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C50);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127C68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127CE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127CF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Cloth", func_00127D10);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1EC8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1EF0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1F10);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1F28);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1F40);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1F58);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A1F70);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A3190);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A31D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A31E8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Cloth", D_003A3200);
