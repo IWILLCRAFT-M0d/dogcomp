0b7d332ba5cfd47d6734572529790089566087f3
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 20:13:34 -0500

    Add split for SwingBridge

---
+++ src/Dogs/Code/SwingBridge.cpp
@@ -0,0 +1,57 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00177788); /* SwingBridge::SwingBridge */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00177898);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00177950);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001779C0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001779E0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001780F0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001782A0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001782F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00178368);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001783D8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C9558);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C9570);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C9598);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C95B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C95D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C95E8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C9600);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003C9618);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003CA838);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003CA878);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003CA890);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SwingBridge", D_003CA8A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00178460);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_001785A0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", __tf11SwingBridge);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00178620);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00178630);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SwingBridge", func_00178638);
