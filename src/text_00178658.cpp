db6218ffaddcc56df851cc4813de44eb7db89072
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 20:10:01 -0500

    Add split for text_00178658 (TugOfWar)

---
+++ src/text_00178658.cpp
@@ -0,0 +1,139 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00178658); /* TugOfWarGod::TugOfWarGod */
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00178818);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_001789D0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00178D38);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00178D58);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00178DA0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00179310);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_001794A0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CAC48);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CAC70);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CACA0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CACF0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_001798B8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_00179AE0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017A110);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017AE20);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017AF88);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017BDE8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017BE28);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C188);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C218);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C238);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C248);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C258);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C2C8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C398);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C538);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C6F0); /* TugOfWarRopeEnd::TugOfWarRopeEnd */
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C7F8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CAF90);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017C818);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017CC98);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017CCB8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CAFD8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB000);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB020);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB038);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB050);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB068);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CB080);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC2A0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC2E0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC2F8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC310);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC5C8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC5F0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC610);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC628);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC640);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC658);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CC670);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CD890);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CD8D0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CD8E8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00178658", D_003CD900);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017CCD8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D0D8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D0E0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D0E8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", __tf11TugOfWarGod);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D168);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D170);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D178);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D1F8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D200);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D2A0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D2F8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D300);
+
+INCLUDE_ASM("asm/nonmatchings/text_00178658", func_0017D320);
