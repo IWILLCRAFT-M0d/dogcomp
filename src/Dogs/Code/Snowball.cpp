09ae6ba4f44dc8a32ee7edcb08d0a0faa4a2155b
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 20:21:20 -0500

    Add splits for SoundCreature, Snowball

---
+++ src/Dogs/Code/Snowball.cpp
@@ -0,0 +1,56 @@
+#include "common.h"
+
+// Snowball
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172A50); /* Snowball::Snowball */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172B48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172BE8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172C08);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172C30);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172C88);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172D90);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172DF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172E20);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172E78);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00172FA8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C5FF0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6018);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6038);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6050);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6068);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6080);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C6098);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C72B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C72F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C7310);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Snowball", D_003C7328);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_00173038);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", __tf8Snowball);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_001731D0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Snowball", func_001731D8);
+
+
---
+++ src/Dogs/Code/SoundCreature.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_001731F8); /* SoundCreature::SoundCreature */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_00173298);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_00173318);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_00173588);
+// vtables
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SoundCreature", D_003C7818);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SoundCreature", D_003C7830);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SoundCreature", D_003C7848);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_00173718);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", __tf13SoundCreature);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_001738B0); /* return D_0044da08; */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SoundCreature", func_001738B8);
+
+
