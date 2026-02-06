74679878519ecf35518cffc531866f5b180bc451
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-06 14:47:39 -0500

    Add split for Trail

---
+++ include/Dogs/Trail.h
@@ -0,0 +1,11 @@
+#ifndef TRAIL_H
+#define TRAIL_H
+
+#include "WorldObj.h"
+
+/*
+class Trail : public WorldObject {
+};
+*/
+
+#endif
---
+++ src/Dogs/Code/Trail.cpp
@@ -0,0 +1,65 @@
+#include "common.h"
+
+#include "Dogs/Trail.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EA50); /* Trail::Trail */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022ED58);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EE48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EE68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EEE0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EF18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EF38);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022EFA8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F2C8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F5D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F6C0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F6F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F740);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F768);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F7B8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F8B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EDB0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EDD8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EDF8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EE10);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EE28);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EE40);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_0042EE58);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_00430078);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_004300B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_004300D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Trail", D_004300E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022F9F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022FA48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022FAA0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Trail", func_0022FAA8);
