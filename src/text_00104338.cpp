f208c74c0a39faf80961b15485c91a348193e432
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-06 17:39:58 -0500

    Add split for text_00104338 (Aperture)

---
+++ src/text_00104338.cpp
@@ -0,0 +1,36 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104338);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104350);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_001043D8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104448);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_001044C0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104668);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_001046A8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104788);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104790);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_001047E8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104A48);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104A88);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", func_00104A90);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00104338", D_00395DD0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00104338", D_00395E18);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00104338", D_00395E60);
+
+INCLUDE_ASM("asm/nonmatchings/text_00104338", __tf12DiscAperture);
+
