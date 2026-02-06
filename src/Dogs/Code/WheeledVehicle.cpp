9171783650ccc03c1c63a4e7ff90071028eb24e6
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 19:59:10 -0500

    Add split for WheeledVehicle.cpp, text_00181948 (WorldObj)

---
+++ src/Dogs/Code/WheeledVehicle.cpp
@@ -0,0 +1,88 @@
+#include "common.h"
+
+#include "Dogs/WheeledVehicle.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_001801A0); /* WheeledVehicle::WheeledVehicle */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_001803E8); /* WheeledVehicle::~WheeledVehicle */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180528);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180530);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180540);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180548);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180A68);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180A98);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00180AF8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181438);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_001815F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", _vt$5WheeledVehicleHitReceiver);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE4C0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE4D8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE500);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE520);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE538);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE550);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE568);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CE580);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CF7A0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CF7E0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CF7F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/WheeledVehicle", D_003CF810);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181618);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181758);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181780);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", __tf14WheeledVehicle);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181800);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181808);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181810);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181818);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181820);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181830);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181838);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181848);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181860);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181870);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", __tf25WheeledVehicleHitReceiver);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_001818C8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_001818D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/WheeledVehicle", func_00181928);
+
