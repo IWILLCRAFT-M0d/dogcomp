32e66ef4e1e4bd3670b8cf56fd0037497157d612
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-06 11:50:21 -0500

    Add Objtable split, text_001ED1D8

---
+++ src/Dogs/Code/Objtable.cpp
@@ -0,0 +1,52 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC630);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC6C8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC6D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC718);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", Objtable_InternalInitialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", Objtable_InternalFinalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", Objtable_Initialise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", Objtable_Finalise__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC8D8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC910);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001EC9F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECA88);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECB28);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECB80);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECD00);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Objtable", D_003E6528); /* "c:/coding/dogs/Code/Common/Objtable.cpp" */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECDD0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECF70);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECFD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ECFF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ED058);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ED0D0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ED188);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/Objtable", func_001ED1B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/Objtable", D_003E6570); /* "Something bad happened in the object table" */
+
