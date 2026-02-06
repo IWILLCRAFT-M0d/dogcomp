963c5c2bdbe1c080fb7e7317f720a6a584fd0151
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-05 21:34:57 -0500

    Add split for text_00127D30 (DiggablePatch)

---
+++ src/text_00127D30.cpp
@@ -0,0 +1,94 @@
+#include "common.h"
+
+// DiggablePatch
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00127D30);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00127E48);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00127EE8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00127F78);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00127F88);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128030);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128120);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128130);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3588);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A35B0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128138);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3660);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3670);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3680);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128498);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_001285B0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_001286D8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128BB0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128C48);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128CA8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128CC8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128CE8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128D80);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128DE0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128E00);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128E98);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128EF8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128F90);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00128FF0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00129010);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_001290A8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A36C0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A36E8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3708);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3720);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3738);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3750);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A3768);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A4988);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A49C8);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A49E0);
+
+INCLUDE_RODATA("asm/nonmatchings/text_00127D30", D_003A49F8);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00129108);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_00129248);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_001292A0);
+
+INCLUDE_ASM("asm/nonmatchings/text_00127D30", func_001292A8);
