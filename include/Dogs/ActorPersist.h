f44a0f6f0a11ba97b1b0e5bd5f381b0165b8c036
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-02-24 11:51:34 -0500

    Add split name for ActorPersist

---
+++ include/Dogs/ActorPersist.h
@@ -0,0 +1,13 @@
+#ifndef ACTORPERSIST_H
+#define ACTORPERSIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void func_0018B978(void);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ src/Dogs/Code/ActorPersist.cpp
@@ -0,0 +1,79 @@
+#include "common.h"
+
+#include "Dogs/ActorPersist.h"
+
+// split (ActorPersist?)
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A208);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A238);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A270); // "ActorPersist: g_allPersistData[] is full"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A2F0); // "ActorPersist: g_allExtrasData is full"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A370);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A398);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A3D8); // "ActorPersist: g_allActorPersistItems[] is full"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A460);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A478);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018A7A8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018AAB0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018AAD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018AB08);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018ABC8); // "Mismatch between current script and stored script"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018AC58); // "Script Header NULL for ActorID = %d"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018ACF0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018AFC8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B010);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B1E0);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B390); // "Assignment to extra of data of wrong size"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B450);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B4F8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B528);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B560);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B5D8); // "_rtnItem is a not allowed to be NULL"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B700);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B728);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B778);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B820);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B910);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B930);
+
+void func_0018B978(void) {
+    return;
+}
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B980);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018B9E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018BB30);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/ActorPersist", func_0018BB50);
+
