54cbf80488034a617d26e6fa266f2c51237112b8
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-03-23 13:38:48 -0400

    Add split for SlidableBlockPuzzle

---
+++ src/Dogs/Code/SlidableBlockPuzzle.cpp
@@ -0,0 +1,70 @@
+#include "common.h"
+#include "unk.h"
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_002282D8); /* SlidableBlockPuzzle */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", _$_19SlidableBlockPuzzle);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_002284D8); // SlidableBlockPuzzle member
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228688); // SlidableBlockPuzzle member
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228728);  // SlidableBlockPuzzle member
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228758);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228C48);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228CD8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228E18);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228E50);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00228F50);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00229110);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00229130);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00229268);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_002292B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C250);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C2B0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C2D8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C2F8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C310);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C328);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C340);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042C358);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042D578);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042D5B8);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042D5D0);
+
+INCLUDE_RODATA("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", D_0042D5E8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_002295B8);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", __tf19SlidableBlockPuzzle);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00229750);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_00229758); /* return D_00450E88 */
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", _$_25PlayerSlideBlockAnimation);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", __tf25PlayerSlideBlockAnimation);
+
+INCLUDE_ASM("asm/nonmatchings/Dogs/Code/SlidableBlockPuzzle", func_002297D0);
