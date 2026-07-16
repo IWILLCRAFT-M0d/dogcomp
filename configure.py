871afdcebab50dbb7a8062519a12230c4cc90011
dogmander <34490166+Dogmander@users.noreply.github.com>
2026-07-16 16:31:10 -0400

    Add split for File_MemCard.cpp

--- configure.py
+++ configure.py
@@ -66,6 +66,7 @@ O0_SPLITS = [
     "src/FGDK3/Code/Playstation2/Music.cpp",
     "src/FGDK3/Code/GameShell.cpp",
     "src/text_002FCF70.cpp",
+    "src/FGDK3/Code/Playstation2/File_MemCard.cpp",
     "src/text_00307D30.cpp",
     "src/FGDK3/Code/FileSystem_Zip.cpp"
 ]
